edc5ba2b7d834ba3957cb8de9c9c7f7a45f0ef07
ShohruhRed <86300104+ShohruhRed@users.noreply.github.com>
2023-07-08 07:34:49 +0000

    test

--- notes.sql
+++ notes.sql
@@ -1,7 +1,7 @@
 -- This file is intended for notes
 -- and for demonstrating how to work
 -- with SQLite in Codespaces.
-
+-- test
 -- These two statements operate on the 
 -- restaurant.db SQLite database.
 SELECT * FROM Dishes;
